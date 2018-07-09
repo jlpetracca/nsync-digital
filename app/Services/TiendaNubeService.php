d6e7df0c4f40c6e31d8d4b821c5b9413c37ae9eb
José Fernando Cordova <josefernando.c@geopagos.com>
2018-07-09 12:19:14 -0300

    update

--- app/Services/TiendaNubeService.php
+++ app/Services/TiendaNubeService.php
@@ -71,20 +71,19 @@ class TiendaNubeService {
 	 */
 	private function saveTnProducts($products){
 		foreach($products->body as $productIndex => $product){
+			//Process tnProduct
+			$this->saveTnProductFieldOnDB($product);
 			//Process and save the attributes array
 			$this->saveTnAttributesOnDBAndGetId($product->attributes);
 			//Process Variants and retrieve values
 			$aValues = $this->saveTnVariantsOnDBAndGetAvalues($product->variants);
 			//Process aValues and assign aId
 			$this->saveAvaluesAndAssignAIdOnDB($aValues);
-			//Process tnVariants and attributes
-			$this->saveTnVariantsAndAttributes();
-			
+			//Process tnImages
+			$this->saveTnImagesOnDB($product->images);
+			//Process tnCategories
+			$this->saveTnCategoriesOnDB($product->categories);
 		}
-	}
-	
-	private function saveTnVariantsAndAttributes(){
-	
 	}
 	
 	/**
@@ -113,7 +112,7 @@ class TiendaNubeService {
 	 * @param $attributes
 	 * @return mixed
 	 */
-	private function saveTnAttributesOnDBAndGetId($attributes){
+	private function saveTnAttributesOnDBAndGetId($attributes = []){
 		if(!empty($attributes)){
 			foreach($attributes as $attributeIndex => $attribute){
 				if(!tnAttributes::where('name', $attribute->es)->first()){
@@ -131,7 +130,7 @@ class TiendaNubeService {
 	 * @param $variants
 	 * @return mixed
 	 */
-	private function saveTnVariantsOnDBAndGetAvalues($variants){
+	private function saveTnVariantsOnDBAndGetAvalues($variants = []){
 		$attributeValues = [];
 		if(!empty($variants)){
 			foreach ($variants as $variantIndex => $variant){
@@ -160,4 +159,74 @@ class TiendaNubeService {
 			return $attributeValues;
 		}
 	}
+	
+	/**
+	 * @param array $images
+	 */
+	private function saveTnImagesOnDB($images = []){
+		if(!empty($images)){
+			foreach($images as $imageIndex => $image){
+				if(!tnProductImages::where('id', $image->id)->first()){
+					tnProductImages::create([
+						'id'            => $image->id,
+						'tn_store_id'   => $this->token['store_id'],
+						'product_id'    => $image->product_id,
+						'src'           => $image->src,
+						'position'      => $image->position,
+						'alt'           => null
+					]);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param array $categories
+	 */
+	private function saveTnCategoriesOnDB($categories = []){
+		if(!empty($categories)){
+			foreach($categories as $categoryIndex => $category){
+				if(!tnCategories::where('id', $category->id)->first()){
+					tnCategories::create([
+						'id'                => $category->id,
+						'tn_store_id'       => $this->token['store_id'],
+						'handle'            => $category->handle->es,
+						'name'              => $category->name->es,
+						'description'       => $category->description->es,
+						'parent'            => $category->parent,
+						'seo_title'         => $category->seo_title->es,
+						'seo_description'   => $category->seo_description->es,
+						//'subcategories'     => !empty($categoryProducts->subcategories) ?
+						//$categoryProducts->subcategories[$categoryProductsIndex] : null,
+						'mage_category_entity_id' => null
+					]);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param $productDetail
+	 */
+	private function saveTnProductFieldOnDB($productDetail){
+		if(!tnProducts::where('id', $productDetail->id)->first()){
+			tnProducts::create([
+				'id'                => $productDetail->id,
+				'tn_store_id'       => $this->token['store_id'],
+				'mage_entity_id'    => null,
+				'name'              => $productDetail->name->es,
+				'description'       => !empty($productDetail->description->es) ? $productDetail->description->es :
+					$productDetail->description->es = "No tiene descripción asociada",
+				'handle'            => $productDetail->handle->es,
+				'published'         => $productDetail->published,
+				'free_shipping'     => $productDetail->free_shipping,
+				'seo_title'         => $productDetail->seo_title->es,
+				'seo_description'   => $productDetail->seo_description->es,
+				'brand'             => $productDetail->brand,
+				'tags'              => $productDetail->tags,
+				'status'            => null,
+			]);
+		}
+	}
+	
 }
